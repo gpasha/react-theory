2f8ec0c96ac072b523daffb80524263ca8b01a7f
gpasha <g1ebikpasha@gmail.com>
2021-07-05 18:35:26 +0300

    dynamic css classes

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import './Car.css'
 
-export default props => (
-    <div className='car'>
-        <h3>Car name: {props.name}</h3>
-        <p>Year: {props.year}</p>
-        <input type='text' onChange={props.changeName} value={props.name} />
-        <div>
-            <button onClick={props.onDelete}>Delete</button>
+export default props => {
+    let classes = ['input']
+
+    if (props.name.length === 0) {
+        classes = [...classes, 'red']
+    }
+    else {
+        classes = [...classes, 'green']
+    }
+
+    if (props.name.length > 5) classes = [...classes, 'bold']
+
+    return (
+        <div className='car'>
+            <h3>Car name: {props.name}</h3>
+            <p>Year: {props.year}</p>
+            <input type='text'
+                   className={classes.join(' ')}
+                   value={props.name}
+                   onChange={props.changeName} />
+            <div>
+                <button onClick={props.onDelete}>Delete</button>
+            </div>
         </div>
-    </div>
-)
+    )
+}
